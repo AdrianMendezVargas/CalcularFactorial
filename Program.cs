8c10b1cd3ae2ddfd678f868546139e0226ee1606
AdrianMendezVargas <velociraptor088@gmail.com>
2020-01-22 18:56:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculaFactorial {
+    class Program {
+        static void Main(string[] args) {
+
+            int numero;
+
+            Console.Write("Factorial de: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n\nEl factorial de " + numero + " es: " + CalcularFactorial(numero));
+
+
+
+        }
+        static long CalcularFactorial(int numero) {
+
+            long factorial = 1;
+            
+            for(int i = 0 ; i < numero ; i++) {
+                factorial *= i + 1;
+            }
+            return factorial;
+
+        }
+    }
+}
